b32f419f2f190701c7c79dbb89117cc5b1ea5631
spark8899 <sparkzhang8899@gmail.com>
2023-03-31 10:15:23 +0800

    delete port param.

--- main.go
+++ main.go
@@ -1,56 +1,55 @@
 package main
 
 import (
-    "context"
-    "flag"
-    "log"
-    "net/http"
-    "os"
-    "os/signal"
-    "strings"
-    "syscall"
-    "time"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 
-    "github.com/spark8899/deploy-agent/pkg/tracer"
+	"github.com/spark8899/deploy-agent/pkg/tracer"
 
-    "github.com/gin-gonic/gin"
-    "github.com/gin-gonic/gin/binding"
-    "github.com/spark8899/deploy-agent/global"
-    "github.com/spark8899/deploy-agent/internal/routers"
-    "github.com/spark8899/deploy-agent/pkg/logger"
-    "github.com/spark8899/deploy-agent/pkg/setting"
-    "github.com/spark8899/deploy-agent/pkg/validator"
-    "gopkg.in/natefinch/lumberjack.v2"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/spark8899/deploy-agent/global"
+	"github.com/spark8899/deploy-agent/internal/routers"
+	"github.com/spark8899/deploy-agent/pkg/logger"
+	"github.com/spark8899/deploy-agent/pkg/setting"
+	"github.com/spark8899/deploy-agent/pkg/validator"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 var (
-    port      string
-    runMode   string
-    config    string
-    isVersion bool
+	config  string
+	version bool
 )
 
 func init() {
-    err := setupFlag()
-    if err != nil {
-        log.Fatalf("init.setupFlag err: %v", err)
-    }
-    err = setupSetting()
-    if err != nil {
-        log.Fatalf("init.setupSetting err: %v", err)
-    }
-    err = setupLogger()
-    if err != nil {
-        log.Fatalf("init.setupLogger err: %v", err)
-    }
-    err = setupValidator()
-    if err != nil {
-        log.Fatalf("init.setupValidator err: %v", err)
-    }
-    err = setupTracer()
-    if err != nil {
-        log.Fatalf("init.setupTracer err: %v", err)
-    }
+	err := setupFlag()
+	if err != nil {
+		log.Fatalf("init.setupFlag err: %v", err)
+	}
+	err = setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+	err = setupLogger()
+	if err != nil {
+		log.Fatalf("init.setupLogger err: %v", err)
+	}
+	err = setupValidator()
+	if err != nil {
+		log.Fatalf("init.setupValidator err: %v", err)
+	}
+	err = setupTracer()
+	if err != nil {
+		log.Fatalf("init.setupTracer err: %v", err)
+	}
 }
 
 // @title deploy-agent
@@ -58,99 +57,109 @@ func init() {
 // @description Cooperate with jenkin to update software and execute restart procedure
 // @termsOfService https://github.com/spark8899
 func main() {
-    gin.SetMode(global.ServerSetting.RunMode)
-    router := routers.NewRouter()
-    s := &http.Server{
-        Addr:           ":" + global.ServerSetting.HttpPort,
-        Handler:        router,
-        ReadTimeout:    global.ServerSetting.ReadTimeout,
-        WriteTimeout:   global.ServerSetting.WriteTimeout,
-        MaxHeaderBytes: 1 << 20,
-    }
+	gin.SetMode(global.ServerSetting.RunMode)
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":" + global.ServerSetting.HttpPort,
+		Handler:        router,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
 
-    go func() {
-        log.Println("Server start running")
-        if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-            log.Fatalf("s.ListenAndServe err: %v", err)
-        }
-    }()
+	go func() {
+		log.Println("Server start running")
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("s.ListenAndServe err: %v", err)
+		}
+	}()
 
-    quit := make(chan os.Signal)
-    signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-    <-quit
-    log.Println("Shuting down server...")
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shuting down server...")
 
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancel()
-    if err := s.Shutdown(ctx); err != nil {
-        log.Fatal("Server forced to shutdown:", err)
-    }
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
 
-    log.Println("Server exiting")
+	log.Println("Server exiting")
 }
 
 func setupFlag() error {
-    flag.StringVar(&port, "port", "", "启动端口")
-    flag.StringVar(&runMode, "mode", "", "启动模式")
-    flag.StringVar(&config, "config", "configs/", "指定要使用的配置文件路径")
-    flag.BoolVar(&isVersion, "version", false, "编译信息")
-    flag.Parse()
+	flag.StringVar(&config, "c", "configs", "指定要使用的配置文件目录")
+	flag.BoolVar(&version, "v", false, "编译信息")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s -c <configs directory>\n", os.Args[0])
+	}
+	flag.Parse()
 
-    return nil
+	if !flag.Parsed() {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if config == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if version {
+		fmt.Printf("%s version is none.\n", os.Args[0])
+		os.Exit(0)
+	}
+
+	return nil
 }
 
 func setupSetting() error {
-    s, err := setting.NewSetting(strings.Split(config, ",")...)
-    if err != nil {
-        return err
-    }
-    err = s.ReadSection("Server", &global.ServerSetting)
-    if err != nil {
-        return err
-    }
-    err = s.ReadSection("App", &global.AppSetting)
-    if err != nil {
-        return err
-    }
+	s, err := setting.NewSetting(strings.Split(config, ",")...)
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
 
-    global.AppSetting.DefaultContextTimeout *= time.Second
-    global.ServerSetting.ReadTimeout *= time.Second
-    global.ServerSetting.WriteTimeout *= time.Second
-    if port != "" {
-        global.ServerSetting.HttpPort = port
-    }
-    if runMode != "" {
-        global.ServerSetting.RunMode = runMode
-    }
+	global.AppSetting.DefaultContextTimeout *= time.Second
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
 
-    return nil
+	return nil
 }
 
 func setupLogger() error {
-    fileName := global.AppSetting.LogSavePath + "/" + global.AppSetting.LogFileName + global.AppSetting.LogFileExt
-    global.Logger = logger.NewLogger(&lumberjack.Logger{
-        Filename:  fileName,
-        MaxSize:   500,
-        MaxAge:    10,
-        LocalTime: true,
-    }, "", log.LstdFlags).WithCaller(2)
+	fileName := global.AppSetting.LogSavePath + "/" + global.AppSetting.LogFileName + global.AppSetting.LogFileExt
+	global.Logger = logger.NewLogger(&lumberjack.Logger{
+		Filename:  fileName,
+		MaxSize:   500,
+		MaxAge:    10,
+		LocalTime: true,
+	}, "", log.LstdFlags).WithCaller(2)
 
-    return nil
+	return nil
 }
 
 func setupValidator() error {
-    global.Validator = validator.NewCustomValidator()
-    global.Validator.Engine()
-    binding.Validator = global.Validator
+	global.Validator = validator.NewCustomValidator()
+	global.Validator.Engine()
+	binding.Validator = global.Validator
 
-    return nil
+	return nil
 }
 
 func setupTracer() error {
-    jaegerTracer, _, err := tracer.NewJaegerTracer("deploy-agent", "127.0.0.1:6831")
-    if err != nil {
-        return err
-    }
-    global.Tracer = jaegerTracer
-    return nil
+	jaegerTracer, _, err := tracer.NewJaegerTracer("deploy-agent", "127.0.0.1:6831")
+	if err != nil {
+		return err
+	}
+	global.Tracer = jaegerTracer
+	return nil
 }
